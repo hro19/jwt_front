79a7c6295486f534868e03f2ebb23b2ff915864d
hori <fourbasic19@gmail.com>
2023-05-23 16:00:12 +0900

    【axios】axiosクライアント作成

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 // import queryString from "query-string"; //URLクエリを取得するため
 
+
 const BASE_URL = "http://localhost:5000/api/v1";
 const getToken = () => localStorage.getItem("token");
 
