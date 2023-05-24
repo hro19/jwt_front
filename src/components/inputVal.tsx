fff6471b61ef510e8905ce1d5072165b301a07bf
hori <fourbasic19@gmail.com>
2023-05-24 18:01:09 +0900

    【problem】passwordとconfirmPasswordが一致しないときはエラーを吐きたい。どう書けばいい？

--- src/components/inputVal.tsx
+++ src/components/inputVal.tsx
@@ -12,7 +12,7 @@ export const validationSchema = z.object({
     .string()
     .nonempty("名前は必須なんだ")
     .min(4, "名前は4文字以上で入力なんだ")
-    .max(12, "名前は12文字以上で入力なんだ"),
+    .max(12, "名前は12文字以下で入力なんだ"),
   password: z
     .string()
     .nonempty("パスワードは必須なんだ")
@@ -21,4 +21,5 @@ export const validationSchema = z.object({
     .string()
     .nonempty("確認パスワードは必須なんだ")
     .min(6, "確認パスワードは6文字以上で入力なんだ"),
+  //passwordとconfirmPasswordが一致しないときはエラーを吐きたい。どう書けばいい？
 });
