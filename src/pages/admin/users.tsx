9ba9e96fcf1f2cb1aaae173eb8773d96d1747ac4
hori <fourbasic19@gmail.com>
2023-08-02 18:39:25 +0900

    【Mui】ボタンのカラーを変更

--- src/pages/admin/users.tsx
+++ src/pages/admin/users.tsx
@@ -71,7 +71,8 @@ const Users = () => {
                 <TableCell>
                   <Button
                     variant="contained"
-                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                    color="success"
+                    className="font-bold py-2 px-4 rounded"
                     onClick={() => handleDelete(user._id)}
                   >
                     削除
