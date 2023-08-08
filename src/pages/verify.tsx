abac58d76a67156c5abee554eda9924c33301e4a
hori <fourbasic19@gmail.com>
2023-08-08 23:54:05 +0900

    【react】ユーザー情報に紐づくtasks一覧を表示

--- src/pages/verify.tsx
+++ src/pages/verify.tsx
@@ -1,24 +1,46 @@
-import React, { useEffect } from 'react';
-import authUtils from '../utils/authUtils';
+import React, { useEffect, useState } from "react";
+import axios from "axios"; // axiosをインポートする
+
+import authUtils from "../utils/authUtils";
 
 const Verify = () => {
+  const [tasks, setTasks] = useState([]); // タスク情報を保持するステート
 
   useEffect(() => {
     const checkAuth = async () => {
-      //ページ切り替える度に認証チェック(トークン持ってるかどうか確認)
-      //ここで404notfoud
       const isAuth = await authUtils.isAuthenticated();
       if (!isAuth) {
         console.log("認証に失敗でござります");
       } else {
         console.log("認証に大大大大大大成功");
-        console.log(isAuth.data.user);
+        const userId = isAuth.data.user._id;
+
+        // ユーザーのタスク情報を取得
+        try {
+          const response = await axios.get(
+            `https://jwt-mongo.vercel.app/api/v1/tasks/${userId}/usertasks`
+          );
+          setTasks(response.data); // タスク情報をステートにセット
+        } catch (error) {
+          console.error("タスク情報の取得に失敗しました", error);
+        }
       }
     };
     checkAuth();
   }, []);
 
-  return <div>トークン認証ページ</div>;
-}
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">タスク一覧</h1>
+      <ul>
+        {tasks.map((task:any) => (
+          <li key={task._id}>
+            {task.name} - {task.completed ? "完了" : "未完了"}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Verify
+export default Verify;
