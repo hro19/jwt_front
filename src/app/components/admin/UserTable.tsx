7d9a4a0580904e3501b58e1f123d7d8d4c553c4e
hori <fourbasic19@gmail.com>
2023-08-15 18:21:12 +0900

    【tailwind】カード調にする

--- src/app/components/admin/UserTable.tsx
+++ src/app/components/admin/UserTable.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const UserTable = ({ user, handleDelete }: any) => {
   return (
-    <div>
-      <h2>{user._id}</h2>
-      <p>【ユーザー名】{user.username}</p>
+    <div className="border rounded p-4 mb-4">
+      <h2 className="text-lg font-semibold">{user._id}</h2>
+      <p className="text-gray-600">【ユーザー名】{user.username}</p>
       <button
-        color="success"
-        className="font-bold py-2 px-4 rounded"
+        className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-2"
         onClick={() => handleDelete(user._id)}
       >
         削除
@@ -16,4 +15,4 @@ const UserTable = ({ user, handleDelete }: any) => {
   );
 };
 
-export default UserTable
+export default UserTable;
