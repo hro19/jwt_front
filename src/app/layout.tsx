a24032fbad40abbf27f1ef1ae39ae117afd49328
hori <fourbasic19@gmail.com>
2023-08-16 19:50:30 +0900

    【tailwind】会員用のボタンを共通化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,14 +19,16 @@ export default function RootLayout({
     <AppProvider>
       <html lang="en">
         <body className={inter.className}>
-          <a href="/register">
-            <h2 className="">register</h2>
-          </a>
-          <a href="/login">
-            <h2 className="">login</h2>
-          </a>
-          <div className="flex flex-col items-center justify-between mb-2">
-            <div className="grid gap-1 text-center max-w-5xl w-full mb-0 grid-cols-5 text-base lg:text-2xl">
+          <div className="flex flex-col justify-between mb-2">
+            <div className="flex justify-end space-x-2 my-1">
+              <a href="/login" className="rounded-full border p-1 bg-lime-300">
+                <h2 className="text-center">login</h2>
+              </a>
+              <a href="/register" className="rounded-full border p-1 bg-lime-300">
+                <h2 className="text-center">register</h2>
+              </a>
+            </div>
+            <div className="grid gap-1 text-center max-w-5xl w-full mb-0 mx-auto grid-cols-5 text-base lg:text-2xl">
               <a href="/">
                 <h2 className={`mb-3 font-semibold text-center bg-slate-200`}>home</h2>
               </a>
