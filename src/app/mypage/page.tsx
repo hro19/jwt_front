2f15f70cf2fa0fde8bc9c253e9a2ede9ad8df210
hori <fourbasic19@gmail.com>
2023-08-17 14:26:21 +0900

    【react】マイページに認証許可用のコンポーネントを設置

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -2,7 +2,9 @@
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import authUtils from "@/utils/authUtils";
+import AuthVerify from "@/utils/AuthVerify";
+import { useAtom } from "jotai";
+import { verifyUserAtom } from "@/jotai/userAtoms";
 
 const fetchUserTasks = async (userId: string) => {
   try {
@@ -18,49 +20,48 @@ const fetchUserTasks = async (userId: string) => {
 const Verify = () => {
   const [tasks, setTasks] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [userId, setUserId] = useState<string | null>(null); // userIdをuseStateで管理
+  const [verifyUser, setVerifyUser] = useAtom(verifyUserAtom);
 
   useEffect(() => {
-    const checkAuth = async () => {
-      const isAuth = await authUtils.isAuthenticated();
-      if (!isAuth) {
-        console.log("認証に失敗でござります");
-      } else {
-        console.log("認証に大大大大大大成功");
-        const userId = isAuth.data.user._id;
-        setUserId(userId);
-        // ユーザーのタスク情報を取得
+    if (verifyUser && verifyUser._id) {
+      const fetchTasks = async () => {
         try {
-          const response = await fetchUserTasks(userId);
+          const response = await fetchUserTasks(verifyUser._id);
           setTasks(response);
           setIsLoading(false);
         } catch (error) {
-          console.error("タスク情報の取得に失敗しました", error);
+          console.error("Error fetching tasks:", error);
+          setIsLoading(false);
         }
-      }
-    };
+      };
 
-    checkAuth();
-  }, []);
+      fetchTasks();
+    }
+  }, [verifyUser]);
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold">タスク一覧</h1>
-      <h3>あなたのユーザーID【{userId}】</h3>
-      {isLoading ? (
-        <p>読み込み中...</p>
-      ) : tasks.length === 0 ? (
-        <p>タスクが登録されておりません</p>
-      ) : (
-        <ul>
-          {tasks.map((task: any) => (
-            <li key={task._id}>
-              {task.name} - {task.completed ? "完了" : "未完了"}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <AuthVerify />
+      <div>
+        <h1 className="text-3xl font-bold">タスク一覧</h1>
+        <div>
+          <h3>あなたのユーザーID【{!isLoading && verifyUser && verifyUser._id}】</h3>
+          {isLoading ? (
+            <p>読み込み中...</p>
+          ) : tasks.length === 0 ? (
+            <p>タスクが登録されておりません</p>
+          ) : (
+            <ul>
+              {tasks.map((task: any) => (
+                <li key={task._id}>
+                  {task.name} - {task.completed ? "完了" : "未完了"}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
