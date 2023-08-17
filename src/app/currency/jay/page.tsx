3ad2c3a71922b72760b4f834970f1aa1433a3f95
hori <fourbasic19@gmail.com>
2023-08-17 11:35:25 +0900

    【react】日本の通貨情報を取得して、一覧を表示させる

---
+++ src/app/currency/jay/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const getCurency = async () => {
+  const response = await fetch("https://www.floatrates.com/daily/jpy.json");
+  const phpCur = await response.json();
+  return phpCur;
+};
+
+const UsersList = async () => {
+  const phpCur = await getCurency();
+  return (
+    <div>
+      <h2 className="text-lg font-bold mt-4">日本から見た通貨一覧</h2>
+      <ul>
+        {phpCur &&
+          Object.entries(phpCur).map(([currencyCode, currencyInfo]: any) => (
+            <li key={currencyCode}>
+              <strong>{currencyCode}</strong> - {currencyInfo.name} ({currencyInfo.rate})
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UsersList;
