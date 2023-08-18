f26ab03ca170289669d73185910241aeef3f0d59
hori <fourbasic19@gmail.com>
2023-08-18 16:20:31 +0900

    【Ts】async関数の返り値に型Promiseを定義

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -5,8 +5,9 @@ import axios from "axios";
 import { useQuery, useMutation } from "react-query";
 import AuthVerify from "@/utils/AuthVerify";
 import UserTable from "@/components/admin/UserTable";
+import { User } from "@/ts/User";
 
-const fetchUsers = async () => {
+const fetchUsers = async (): Promise<User[]> => {
   const response = await axios.get("https://jwt-mongo.vercel.app/api/v1/users");
   return response.data;
 };
@@ -50,7 +51,7 @@ const AdminUsers = () => {
       <div className="container mx-auto my-4">
         <h2 className="text-3xl font-bold text-center mb-4">ユーザー一覧</h2>
         <div className="grid gap-4 mx-2 grid-cols-1 lg:grid-cols-3 lg:mx-3">
-          {users.map((user: any) => (
+          {users && users.map((user: User) => (
             <UserTable user={user} handleDelete={handleDelete} key={user._id} />
           ))}
         </div>
