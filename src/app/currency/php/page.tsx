509707ccc863f52e25d2b3853b79335c33a7645d
hori <fourbasic19@gmail.com>
2023-08-21 16:12:08 +0900

    【Ts】Promise、Awaitedを定義

--- src/app/currency/php/page.tsx
+++ src/app/currency/php/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { Currency,CurrencyObj } from "@/ts/Currency";
 
-const getCurency = async () => {
+const getCurency = async (): Promise<CurrencyObj> => {
   const response = await fetch("https://www.floatrates.com/daily/php.json", {
     cache: "no-store",
   });
@@ -9,17 +10,20 @@ const getCurency = async () => {
 };
 
 const UsersList = async () => {
-  const phpCur = await getCurency();
+  const phpCur: Awaited<Promise<CurrencyObj>> = await getCurency();
   return (
     <div>
       <h2 className="text-lg font-bold mt-4">フィリピンから見た通貨一覧</h2>
       <ul>
         {phpCur &&
-          Object.entries(phpCur).map(([currencyCode, currencyInfo]: any) => (
-            <li key={currencyCode}>
-              <strong>{currencyCode}</strong> - {currencyInfo.name} ({currencyInfo.rate})
-            </li>
-          ))}
+          Object.entries(phpCur).map(
+            ([currencyCode, currencyInfo]: [string, Currency]) => (
+              <li key={currencyCode}>
+                <strong>{currencyCode}</strong> - {currencyInfo.name} ({currencyInfo.rate}
+                )
+              </li>
+            )
+          )}
       </ul>
     </div>
   );
