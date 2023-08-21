f10577fe2424a56b953247d15211fbaad36a70c0
hori <fourbasic19@gmail.com>
2023-08-21 20:00:30 +0900

    【js】事前に選択した国だけを抽出

--- src/components/currency/CurrencyWrap.tsx
+++ src/components/currency/CurrencyWrap.tsx
@@ -8,13 +8,37 @@ import {
   CouCurrncyAtom,
 } from "@/jotai/curracyAtoms";
 
+//連想配列から配列に変換する関数
+const AssociativeConvert = (currencyObjData: CurrencyObj): Currency[] => {
+  return Object.values(currencyObjData);
+};
+
+// AbleChooseCountriesでフィルタリングする関数を定義
+const filterAbleChooseCountries = (
+  Currencys: Currency[],
+  AbleChooseCountries: string[]
+): Currency[] => {
+  const upperCaseChooseCountries = AbleChooseCountries.map((code) => code.toUpperCase());
+
+  return Currencys.filter((currencyInfo) =>
+    upperCaseChooseCountries.includes(currencyInfo.code)
+  );
+};
+
 const CurrencyWrap = ({ currencyObjData }: { currencyObjData: CurrencyObj }) => {
   const [couCurrncy, setCouCurrncy] = useAtom(CouCurrncyAtom);
 
+  // CurrencyWrapコンポーネント内でのuseEffectの中身
   useEffect(() => {
-    const filteredCurrenciesArray: Currency[] = Object.entries(currencyObjData)
-      .map(([, currencyInfo]) => currencyInfo); // 明示的な型変換
-    // console.table(filteredCurrenciesArray);
+    const associativeConvertArr: Currency[] = AssociativeConvert(currencyObjData);
+
+    // AbleChooseCountriesでフィルタリングする関数を呼び出す
+    const filteredCurrenciesArray = filterAbleChooseCountries(
+      associativeConvertArr,
+      AbleChooseCountries
+    );
+
+    console.table(filteredCurrenciesArray);
     setCouCurrncy(filteredCurrenciesArray);
   }, [currencyObjData]);
 
