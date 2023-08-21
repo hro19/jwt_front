0bf7f09c09f6bcef28b6d54279d741aa5d1f1172
hori <fourbasic19@gmail.com>
2023-08-21 18:27:22 +0900

    【js】取得した連想配列データを配列データにしてレンダリング

--- src/components/currency/CurrencyWrap.tsx
+++ src/components/currency/CurrencyWrap.tsx
@@ -6,55 +6,16 @@ import { useAtom } from "jotai";
 import {
   AbleChooseCountries,
   CouCurrncyAtom,
-  currencyMainKey,
 } from "@/jotai/curracyAtoms";
-
-//連想配列を配列に変換する（途中で国のピッキングする）
-const filterCurrencies = (
-  currencyObjData: CurrencyObj,
-  AbleChooseCountries: string[]
-): Currency[] => {
-  return Object.entries(currencyObjData)
-    .filter(([currencyCode]) => filterCurrencyCode(currencyCode, AbleChooseCountries))
-    .map(([, currencyInfo]) => currencyInfo);
-};
-
-//事前に選ばれた国かをチェックする
-const filterCurrencyCode = (
-  currencyCode: string,
-  AbleChooseCountries: string[]
-): boolean => {
-  return AbleChooseCountries.includes(currencyCode);
-};
-
-//メインとするキーのみ表示させる
-const filterMainInfo = (
-  currencyInfo: Currency,
-  currencyMainKey: (keyof Currency)[]
-): Partial<Currency> => {
-  const filteredCurrencyInfo: Record<any, any> = {};
-  for (const key of currencyMainKey) {
-    filteredCurrencyInfo[key] = currencyInfo[key];
-  }
-  return filteredCurrencyInfo;
-};
 
 const CurrencyWrap = ({ currencyObjData }: { currencyObjData: CurrencyObj }) => {
   const [couCurrncy, setCouCurrncy] = useAtom(CouCurrncyAtom);
 
   useEffect(() => {
-    const filteredCurrenciesArray: Currency[] = filterCurrencies(
-      currencyObjData,
-      AbleChooseCountries
-    );
-
-
-    const mainCurrenciesArray = filteredCurrenciesArray.map((mainInfoObj) =>
-      filterMainInfo(mainInfoObj, currencyMainKey)
-    );
-    
+    const filteredCurrenciesArray: Currency[] = Object.entries(currencyObjData)
+      .map(([, currencyInfo]) => currencyInfo); // 明示的な型変換
     // console.table(filteredCurrenciesArray);
-    setCouCurrncy(mainCurrenciesArray);
+    setCouCurrncy(filteredCurrenciesArray);
   }, [currencyObjData]);
 
   return (
@@ -63,7 +24,9 @@ const CurrencyWrap = ({ currencyObjData }: { currencyObjData: CurrencyObj }) => 
         {couCurrncy &&
           couCurrncy.map((currencyInfo: Currency, index: number) => (
             <li key={index}>
-              <p>【コード】{currencyInfo.code} -【名前】{currencyInfo.name}</p>
+              <p>
+                【コード】{currencyInfo.code} -【名前】{currencyInfo.name}
+              </p>
               <p>【レート】{currencyInfo.rate}</p>
               <p>【日付】{currencyInfo.date.toString()}</p>
               <p>【両替レート】{currencyInfo.inverseRate}</p>
