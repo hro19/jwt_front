60fa9872261c3339a7e14cb0d5f2e7df2babb05a
hori <fourbasic19@gmail.com>
2023-08-24 14:47:27 +0900

    【js】数字入力だけを可能にしたinputのたたき台

---
+++ src/app/inputnumber/page.tsx
@@ -0,0 +1,54 @@
+//参考サイト https://zenn.dev/funteractiveinc/articles/component-input-number
+
+"use client";
+
+import React, { useState, FormEvent } from "react";
+
+type Calculation = {
+  base_value: number;
+};
+
+const page = () => {
+  const [calculation, setCalculation] = useState<Calculation>({
+    base_value: 0,
+  });
+
+  const onChangeHandler = (value: string) => {
+    const v = value.replace(/[０-９．]/g, (s) =>
+      String.fromCharCode(s.charCodeAt(0) - 0xfee0)
+    );
+    if (!isNaN(Number(v))) {
+     setBaseValue(Number(v)); // 関数名を修正
+    }
+  };
+
+  const setBaseValue = (value: number) => {
+    setCalculation({ base_value: value });
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault(); // イベントのデフォルト動作をキャンセルする
+    console.table(calculation);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="my-4">
+      <input
+        type="text"
+        inputMode="numeric"
+        value={calculation.base_value}
+        onChange={(e) => onChangeHandler(e.target.value)}
+        className="w-48 px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+      />
+      <button
+        type="submit"
+        className="bg-emerald-400 text-white px-4 py-2 rounded-lg ml-2 hover:bg-emerald-600"
+      >
+        レート計算
+      </button>
+    </form>
+  );
+};
+
+export default page
+
