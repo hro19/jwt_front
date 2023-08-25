476ff22684665ccd2c77837ba00cc07b4e8e44f0
hori <fourbasic19@gmail.com>
2023-08-25 14:24:07 +0900

    【tailwind】日本両替時のcss調整

--- src/components/currency/CurrencyWrap.tsx
+++ src/components/currency/CurrencyWrap.tsx
@@ -30,37 +30,43 @@ const CurrencyWrap = ({ currencyObjData }: { currencyObjData: CurrencyObj }) => 
     <>
       {couCurrncy && (
         <div className="flex">
-          <div className="relative w-full bg-cyan-100 px-4 pb-12 pt-2">
-            <i className="absolute -top-8 -left-3">
+          <div className="relative w-full bg-cyan-100 px-4 py-12">
+            <i className="absolute top-3 left-2">
               <Image
                 src="/country/php.png"
                 alt="交換元通貨のフラグ"
-                width={100}
-                height={70}
+                width={80}
+                height={55}
                 priority
               />
             </i>
-            <h2 className="text-md font-bold text-right">
-              {/* {couCurrncy["php"].code}のコンテンツ */}
-              {currencyJaName[couCurrncy["php"].code]}
-            </h2>
             <section className="text-center">
-              <h3>レート:{couCurrncy["php"].inverseRate.toFixed(5)}</h3>
+              <h3>
+                {currencyJaName[couCurrncy["php"].code]}のレート<br />
+                {couCurrncy["php"].inverseRate.toFixed(5)}
+              </h3>
               <p className="text-xs">更新日（{dateUntilFun(couCurrncy["php"].date)}）</p>
               <input type="text" id="calculation" className="my-4 text-2xl" />
             </section>
           </div>
-          <div className="relative w-full bg-fuchsia-100 px-4 pb-12 pt-2">
-            <i className="absolute -top-8 -left-3">
+          <div className="relative w-full bg-fuchsia-100 px-4 py-12">
+            <i className="absolute top-3 left-2">
               <Image
                 src="/country/jpy.png"
                 alt="交換目的通貨のフラグ"
-                width={100}
-                height={70}
+                width={80}
+                height={55}
                 priority
               />
             </i>
-            <h2 className="text-md font-bold text-right">日本円のコンテンツ</h2>
+            {/* <h2 className="text-md font-bold text-right">日本円のコンテンツ</h2> */}
+            <section className="grid place-content-center place-items-center h-full">
+              <h3 className="mb-3">両替時の日本円</h3>
+              <p className="text-4xl">
+                {Math.floor(432300).toLocaleString("ja-JP")}
+                <span className="text-sm">円</span>
+              </p>
+            </section>
           </div>
         </div>
       )}
