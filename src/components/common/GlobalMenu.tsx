d13d349484dc54ba73f663cd560340940b37c2ce
hori <fourbasic19@gmail.com>
2023-09-29 03:09:26 +0900

    【react】通貨関連ページを削除

--- src/components/common/GlobalMenu.tsx
+++ src/components/common/GlobalMenu.tsx
@@ -14,20 +14,20 @@ const gMenus = [
     href: "/mypage",
     text: "マイページ",
   },
-  {
-    href: "/currency/php",
-    text: "Ph通貨",
-  },
-  {
-    href: "/currency/jay",
-    text: "通貨計算",
-  },
+  // {
+  //   href: "/currency/php",
+  //   text: "Ph通貨",
+  // },
+  // {
+  //   href: "/currency/jay",
+  //   text: "通貨計算",
+  // },
 ];
 
 const GlobalMenu = () => {
   return (
     <div className="bg-slate-200 mb-3">
-      <div className="grid gap-1 text-center max-w-5xl w-full mx-auto grid-cols-5 text-base lg:text-xl">
+      <div className="grid gap-1 text-center max-w-5xl w-full mx-auto grid-cols-3 text-base lg:text-xl">
         {gMenus.map((menu, index) => (
           <Link
             key={index}
