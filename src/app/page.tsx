fa6e4f2ef4dc8f00279ca9b759b92a326131cee7
hori <fourbasic19@gmail.com>
2023-09-29 16:51:24 +0900

    【chakraUI】ロゴをユーティリティに設置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative flex place-items-center before:absolute ">
-        <Image
-          className=""
-          src="/logo.png"
-          alt="フォームロゴ"
-          width={180}
-          height={37}
-          priority
-        />
+            dddd
       </div>
 
     </main>
--- src/components/common/UtilityMenu.tsx
+++ src/components/common/UtilityMenu.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import { Box, Flex, HStack, Spacer } from "@chakra-ui/react";
 
 const uMenus = [
   {
@@ -18,17 +20,25 @@ const uMenus = [
 
 const UtilityMenu = () => {
   return (
-    <div className="flex justify-end space-x-2 my-2 mr-2">
-      {uMenus.map((menu, index) => (
-        <Link
-          key={index}
-          href={menu.href}
-          className="inline-flex items-center justify-center px-4 py-1 text-sm font-bold leading-6 text-white bg-indigo-600 border border-transparent rounded-full w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
-        >
-          <h2 className="text-center">{menu.text}</h2>
-        </Link>
-      ))}
-    </div>
+    <Flex alignItems={"center"}>
+      <Box mb="1">
+        <Image className="" src="/logo.png" alt="ロゴ" width={180} height={37} priority />
+      </Box>
+      <Spacer />
+      <Box mr="3">
+        <HStack spacing={1}>
+          {uMenus.map((menu, index) => (
+            <Link
+              key={index}
+              href={menu.href}
+              className="inline-flex items-center justify-center px-4 py-1 text-sm font-bold leading-6 text-white bg-indigo-600 border border-transparent rounded-full w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+            >
+              <h2 className="text-center">{menu.text}</h2>
+            </Link>
+          ))}
+        </HStack>
+      </Box>
+    </Flex>
   );
 };
 
