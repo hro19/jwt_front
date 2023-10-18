4bf87725ac93203c3804c6fc17ffe8555c628552
hori <fourbasic19@gmail.com>
2023-10-18 21:06:42 +0900

    【chakraui】送信時のスピナーの設置と、完了後にメッセージを表示

--- src/app/uploader2/page.tsx
+++ src/app/uploader2/page.tsx
@@ -1,27 +1,49 @@
 "use client";
 
-import { Box, Button, Heading,Flex, FormControl, FormLabel } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Heading,
+  Flex,
+  FormControl,
+  FormLabel,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import { useState } from "react";
 
 function App() {
   const [file, setFile] = useState("");
   const [isUploaded, setIsUploaded] = useState(false);
+  const [reply, setReply] = useState<any>();
 
   const handleChange = (e: any) => {
     setFile(e.target.files[0]);
   };
 
-  const handleSubmit = async (e:any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
+    setIsUploaded(true);
     const formData = new FormData();
     formData.append("file", file);
-    const response = await fetch("https://express-cloudflarer2.vercel.app/api/upload", {
-      method: "POST",
-      body: formData,
-    });
 
-    const reply = await response.json();
-    console.log(reply.message);
+    try {
+      const response = await fetch("https://express-cloudflarer2.vercel.app/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setReply(data); // レスポンスデータを reply ステートにセット
+      } else {
+        console.error("HTTPエラー:", response.status);
+      }
+    } catch (error) {
+      console.error("エラー:", error);
+    }
+
+    setIsUploaded(false);
   };
 
   return (
@@ -34,7 +56,6 @@ function App() {
       <Box>
         <form onSubmit={handleSubmit}>
           <Box mb={"4"}>
-            <FormLabel htmlFor="file">Select file:</FormLabel>
             <input type="file" name="file" onChange={handleChange} />
           </Box>
           <Flex>
@@ -49,6 +70,8 @@ function App() {
           </Flex>
         </form>
       </Box>
+      <Box my={"4"}>{isUploaded ? <Spinner color="red.500" /> : ""}</Box>
+      <Text fontSize={"3xl"}>{ reply && reply.message}</Text>
     </>
   );
 }
