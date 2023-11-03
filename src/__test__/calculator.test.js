67e606570522ec7b97cfcd19703cd41804b42aa7
hori <fourbasic19@gmail.com>
2023-11-03 12:44:51 +0900

    【jest】配列とオブジェクトのテスト基礎

--- src/__test__/calculator.test.js
+++ src/__test__/calculator.test.js
@@ -1,6 +1,6 @@
-import {sum} from "./calculator";
+import { sum } from "./calculator";
 
-test('基本的な加算式', () => {
+test("基本的な加算式", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
@@ -21,7 +21,7 @@ test("6と6を足す足し算", () => {
 });
 
 test("3つの値を足す式", () => {
-  expect(sum(6,6,6)).toBe(18);
+  expect(sum(6, 6, 6)).toBe(18);
 });
 
 test("4つの値を足す式", () => {
@@ -30,4 +30,89 @@ test("4つの値を足す式", () => {
 
 test("引数5個で足し算計算", () => {
   expect(sum(7, 7, 7, 7, 7)).toBe(35);
-})
+});
+
+const teamsMember = {
+  name: "馬場",
+  age: 33,
+  position: "捕手",
+};
+
+test("オブジェクトのテストその1", () => {
+  expect(teamsMember).toStrictEqual({
+    name: "馬場",
+    age: 33,
+    position: "捕手",
+  });
+});
+
+test("オブジェクトのテストその2", () => {
+  expect(teamsMember).toMatchObject({
+    name: "馬場",
+    age: 33,
+  });
+});
+
+const yakyumembers = ["馬場", "相島", "堤田"];
+
+test("配列のテストその1", () => {
+  expect(yakyumembers).toStrictEqual(["馬場", "相島", "堤田"]);
+});
+
+test("配列のテストその2", () => {
+  expect(yakyumembers).not.toStrictEqual(["馬場", "相島"]);
+});
+
+test("配列のテストその3", () => {
+  expect(yakyumembers).toEqual(expect.arrayContaining(["馬場", "相島"]));
+});
+
+const members = [
+  {
+    name: "馬場",
+    age: 33,
+    position: "捕手",
+  },
+  {
+    name: "星野",
+    age: 53,
+    position: "投手",
+  },
+  {
+    name: "木下",
+    age: 29,
+    position: "内野手",
+  },
+];
+
+test("配列の中のオブジェクトのテストその1", () => {
+  expect(yakyumembers).not.toStrictEqual([
+    {
+      name: "馬場",
+      age: 33,
+      position: "捕手",
+    },
+    {
+      name: "星野",
+      age: 53,
+      position: "投手",
+    },
+    {
+      name: "木下",
+      age: 29,
+      position: "内野手",
+    },
+  ]);
+});
+
+test("配列の中のオブジェクトのテストその2", () => {
+  expect(members).toEqual(
+    expect.arrayContaining([
+      {
+        name: "馬場",
+        age: 33,
+        position: "捕手",
+      },
+    ])
+  );
+});
