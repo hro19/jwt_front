5943296b39ece314f9cf618b263fdc0922618df1
hori <fourbasic19@gmail.com>
2023-11-03 13:45:09 +0900

    【jest】spyOnで返り値を変更する

--- src/api/apiTask.test.ts
+++ src/api/apiTask.test.ts
@@ -20,11 +20,61 @@ describe("validateResponse", () => {
 import { enableFetchMocks } from "jest-fetch-mock";
 enableFetchMocks();
 
-describe("fetchTasks", () => {
+describe("fetchTasksその1-1", () => {
   it("レスポンス確認", async () => {
     const response = await fetchTasks();
     // expect(response).toBeDefined();
     expect(response.status).toBe(200);
+  });
+});
+
+// describe("fetchTasksその1-2", () => {
+//   it("レスポンス確認", async () => {
+//     const response = await fetchTasks();
+//     expect(response.status).toBe(200);
+//     expect(response).toBeGreaterThan(0); // 配列の長さが1以上か確認
+//   });
+// });
+
+describe("fetchTasksその2", () => {
+  it("APIリクエストに成功する場合", async () => {
+    const mockData = [
+      {
+        _id: "6464a33a2d30f0fedd9c0f29",
+        name: "床屋に髪を切りに行く",
+        completed: true,
+        __v: 0,
+        userId: "64ca465b59acf1aa11d7152b",
+      },
+      {
+        _id: "64ca18514bcc139cb730cae2",
+        name: "自転車を修理に",
+        completed: false,
+        userId: "64ca465b59acf1aa11d7152b",
+      },
+    ];
+
+    const mockResponse = new Response(JSON.stringify(mockData), {
+      status: 200,
+      headers: { "Content-type": "application/json" },
+    });
+
+    jest.spyOn(global, "fetch").mockResolvedValue(mockResponse);
+
+    const response = await fetchTasks();
+    const responseData = await response.json();
+    expect(response.status).toBe(200);
+    expect(responseData).toEqual(mockData);
+  });
+});
+
+describe("fetchTasksその3", () => {
+  it("APIリクエストが失敗する場合", () => {
+    jest
+      .spyOn(global, "fetch")
+      .mockReturnValue(Promise.reject(new Error("Network Error")));
+
+    return expect(fetchTasks()).rejects.toThrow("Network Error");
   });
 });
 
