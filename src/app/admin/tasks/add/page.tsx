fab0ba8384a67776eace63b334589954496af136
hori <fourbasic19@gmail.com>
2023-11-17 16:09:03 +0900

    【chakra,rhf】で新規taskフォームを設置

--- src/app/admin/tasks/add/page.tsx
+++ src/app/admin/tasks/add/page.tsx
@@ -1,39 +1,51 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { Task } from "@/ts/Task";
 import { useQuery, useMutation, QueryClient } from "@tanstack/react-query";
-import AuthVerify from "@/utils/AuthVerify";
-import TaskTable from "@/components/admin/TaskTable";
 import apiTask from "@/api/apiTask";
+import { useForm } from "react-hook-form";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+
+type FormData = {
+  name: string;
+  completed: boolean;
+  userId: string;
+};
 
 const queryClient = new QueryClient();
 
-const formData: any = {
-  name: "電車に乗る14",
-  completed: true,
-  userId: "64ca465b59acf1aa11d7152b",
-};
-
 const CreateTodo = () => {
-
   const {
-    data,
-    isLoading,
-    error,
-    refetch,
-  } = useQuery({ queryKey: ["tasks"], queryFn: apiTask.getAll });
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const { data, isLoading, error, refetch } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: apiTask.getAll,
+  });
 
   const mutation = useMutation({
     mutationFn: apiTask.add,
     onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: ["tasks"] });
-        refetch();
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      refetch();
     },
   });
 
-  const onCreateTodo = (e: any) => {
-    e.preventDefault();
+  const onSubmit = (formData: any) => {
     mutation.mutate(formData);
   };
 
@@ -47,18 +59,56 @@ const CreateTodo = () => {
 
   return (
     <>
-      <button
-        onClick={() => {
-          mutation.mutate(formData);
-        }}
-      >
-        新規タスク
-      </button>
+      <Box>
+        <Container maxW="680px">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={3}>
+              <FormControl>
+                <FormLabel htmlFor="name">Name</FormLabel>
+                <Input
+                  type="text"
+                  id="name"
+                  defaultValue="電車に乗る14"
+                  {...register("name")}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="completed">Completed</FormLabel>
+                <Checkbox id="completed" defaultChecked {...register("completed")} />
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="userId">User ID</FormLabel>
+                <Input
+                  type="text"
+                  id="userId"
+                  defaultValue="64ca465b59acf1aa11d7152b"
+                  {...register("userId")}
+                />
+              </FormControl>
+              <Button
+                bg="white"
+                border="1px"
+                borderColor="blue.500"
+                _hover={{ bg: "blue.100" }}
+                fontSize="lg"
+                type="submit"
+                w={32}
+              >
+                新規タスク
+              </Button>
+            </Stack>
+          </form>
+        </Container>
+      </Box>
       <hr />
       <hr />
-      <div className="grid gap-4 mx-2 grid-cols-1 lg:grid-cols-3 lg:mx-3">
+      <div className="grid gap-4 mx-2 grid-cols-1 lg:grid-cols-3 lg:mx-3 mt-6">
         {data.map((task: Task) => (
-          <li key={task._id}>{task.name}</li>
+          <dl key={task._id}>
+            <dt>{task._id}</dt>
+            <dd>{task.name}</dd>
+            <dd>{task.completed ? "完了" : "未着手"}</dd>
+          </dl>
         ))}
       </div>
     </>
